950024c5069c346d56fd9bcea1d10d74a12f184c
Konrad Adamczyk <konradadamczyk@users.sourceforge.net>
2011-09-20 20:41:51 +0200

    P2PM-Tests: missing command scripts for pldep

---
+++ miscellaneous/scripts/pldep/sshcmd.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+while getopts ":c:u:h:" opt; do
+  case $opt in
+    c) command=$OPTARG ;;
+    u) username=$OPTARG ;;
+    h) hostname=$OPTARG ;;
+    \?) exit 1 ;;
+    :) exit 1 ;;
+  esac
+done
+
+if [ -f ~/.agent.env ] ; then
+    . ~/.agent.env > /dev/null
+    if ! kill -0 $SSH_AGENT_PID > /dev/null 2>&1; then
+        eval `ssh-agent | tee ~/.agent.env` > /dev/null 2>&1
+        ssh-add ~/.ssh/id_?*[^pub$]? > /dev/null 2>&1
+    fi
+else
+    eval `ssh-agent | tee ~/.agent.env` > /dev/null 2>&1
+    ssh-add ~/.ssh/id_?*[^pub$]? > /dev/null 2>&1
+fi
+
+ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $username@$hostname "$command"
